1f87a3f2dadf82704ffa45ce995b230a85b68e28
detektivschnurli <134784895+detektivschnurli@users.noreply.github.com>
2024-01-17 17:09:30 +0100

    Update cat.js

--- hm-wip/cat.js
+++ hm-wip/cat.js
@@ -1,5 +1,5 @@
 const bubble = document.getElementById("bubble");
-const text = ["Not right meow.<br>I'm working", "Fuck off.", "They're paying me too little for this.", "rrrrrrrrr", "I fucking hate working here.", "hueremichi?<br>In this economy??", "my favourite movie is cats.", "who ate my canned anchovies??", "Yeah, nyan cat is my brother. wdya?", "Don't touch me!!"];
+const text = ["Not right meow.<br>I'm working", "Fuck off.", "They're paying me too little for this.", "rrrrrrrrr", "I fucking hate working here.", "hueremichi?<br>In this economy??", "My favourite movie is cats.", "Who ate my canned anchovies??", "Yeah, nyan cat is my brother. wdya?", "Don't touch me!!"];
 
 function bubbleText() {
   const randomIndex = Math.floor(Math.random() * text.length);
